d2a310b1db7c068414f19b47f47dddc7d45da1cf
yakushiji <yaoshisizhenghe@gmail.com>
2013-08-06 12:07:27 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+  root  'static_pages#home'
   get "static_pages/home"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
