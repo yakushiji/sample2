2a95af64f7348f7101270f124116856d888a626c
yakushiji <yaoshisizhenghe@gmail.com>
2013-08-07 09:51:32 +0900

    Set sass

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
-  root  'static_pages#home'
   get "static_pages/home"
   get "static_pages/help"
+  root  'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
